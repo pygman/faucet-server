4982cfa1f0504fc1513845a15bb7dbb0b31a60f9
cosinyouth <cosinyouth@163.com>
2020-12-10 11:40:03 +0800

    feat: use cors to fix cross-origin problem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const {initContract, claimTestToken} = require('./utils/contract')
 const {validClaimArgs} = require('./utils/address')
@@ -14,11 +14,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+app.use(cors()) // Use this after the variable declaration
+
 
 // serve static assets normally
 app.use(express.static(__dirname + '/build'));
